f9865b8b8ad86b7998ace4d7142a28ccff68857e
Xa-C24 <9335@holbertonstudents.com>
2025-04-02 15:23:52 +0200

    path page Equipe

--- mon-projet/src/router/index.js
+++ mon-projet/src/router/index.js
@@ -3,12 +3,15 @@ import Accueil from '../components/Accueil.vue'
 import Constat from '../components/Constat.vue'
 import Solutions from '../components/Solutions.vue'
 import Partenaire from '../components/Partenaire.vue'
+import Equipe from '../components/Equipe.vue'
 
 const routes = [
   { path: '/', name: 'Accueil', component: Accueil },
   { path: '/constat', name: 'Constat', component: Constat },
   { path: '/Solutions', name: 'Solutions', component: Solutions },
   { path: '/partenaires', name: 'Partenaires', component: Partenaire },
+  { path: '/equipe', name: 'Equipe', component: Equipe },
+  
 ]
 
 const router = createRouter({
